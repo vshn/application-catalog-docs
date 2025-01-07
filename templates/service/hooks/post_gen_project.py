5587682f434cb814ee1425bd5b7011a60f82ad19
Tobias Brunner <tobias.brunner@vshn.ch>
2025-01-07 10:09:07 +0100

    improve service template

--- templates/service/hooks/post_gen_project.py
+++ templates/service/hooks/post_gen_project.py
@@ -6,13 +6,7 @@ destination_directory = "docs/modules/ROOT/pages/service"
 # Current directory is a directory where a cookiecutter template was rendered
 current_directory = os.getcwd()
 
-# copytree copies all the contents from current and all nested directories
-# from source folder to a destination folder.
-shutil.copytree(
-    current_directory,
-    f"{os.path.dirname(current_directory)}/{destination_directory}",
-    dirs_exist_ok=True,
+# Move the entire current directory to the destination directory
+shutil.move(
+    current_directory, f"{os.path.dirname(current_directory)}/{destination_directory}"
 )
-
-# After copying the files we can remove the source directory
-shutil.rmtree(current_directory)
